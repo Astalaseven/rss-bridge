467eb42c12fdda2beca07e6d071f0df6a3265b8d
Astalaseven <gatien.bovyn@gmail.com>
2013-12-16 08:59:38 +0100

    [enh] Remove commented line

--- bridges/DansTonChatBridge.php
+++ bridges/DansTonChatBridge.php
@@ -19,7 +19,6 @@ class DansTonChatBridge extends BridgeAbstract{
                 $item = new \Item();
                 $item->uri = $element->find('a', 0)->href;
                 $item->title = 'DansTonChat '.$element->find('a', 1)->plaintext;
-                //$item->content = $element->find('p.item-content', 0)->innertext;
                 $item->content = $element->find('a', 0)->innertext;
                 $this->items[] = $item;
         }
